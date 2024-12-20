3b191f2826b59a13e5f32aac04fb09b8984b9034
KwonHyeji <kwonhyeji007@gmail.com>
2024-12-20 20:22:08 +0900

    2024-12-11 class

--- src/main/java/com/green/greengram/feed/comment/model/FeedCommentDto.java
+++ src/main/java/com/green/greengram/feed/comment/model/FeedCommentDto.java
@@ -4,7 +4,8 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
-//Data Transfer Object
+//Data Transfer Object: 컨트롤러, 서비스, 또는 클라이언트로 데이터를 전달할 때
+// DTO가 중간 매개체 역할을 함
 @Getter
 @Setter
 public class FeedCommentDto {
